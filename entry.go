06ac1549b558a17e9645a3f659c2e4fb012e4a32
ThePiachu <ThePiachu@gmail.com>
2016-05-03 13:13:14 -0700

    Added more missing functions.

--- entry.go
+++ entry.go
@@ -10,6 +10,7 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	ed "github.com/FactomProject/ed25519"
 )
 
 type Entry struct {
@@ -148,4 +149,126 @@ func (e *Entry) UnmarshalJSON(data []byte) error {
 	e.Content = p
 
 	return nil
+}
+
+func ComposeEntryCommit(pub *[32]byte, pri *[64]byte, e *Entry) ([]byte, error) {
+	type commit struct {
+		CommitEntryMsg string
+	}
+
+	buf := new(bytes.Buffer)
+
+	// 1 byte version
+	buf.Write([]byte{0})
+
+	// 6 byte milliTimestamp (truncated unix time)
+	buf.Write(milliTime())
+
+	// 32 byte Entry Hash
+	buf.Write(e.Hash())
+
+	// 1 byte number of entry credits to pay
+	if c, err := entryCost(e); err != nil {
+		return nil, err
+	} else {
+		buf.WriteByte(byte(c))
+	}
+
+	// sign the commit
+	sig := ed.Sign(pri, buf.Bytes())
+
+	// 32 byte Entry Credit Public Key
+	buf.Write(pub[:])
+
+	// 64 byte Signature
+	buf.Write(sig[:])
+
+	com := new(commit)
+	com.CommitEntryMsg = hex.EncodeToString(buf.Bytes())
+	j, err := json.Marshal(com)
+	if err != nil {
+		return nil, err
+	}
+
+	return j, nil
+}
+
+func ComposeEntryReveal(e *Entry) ([]byte, error) {
+	type reveal struct {
+		Entry string
+	}
+
+	r := new(reveal)
+	if p, err := e.MarshalBinary(); err != nil {
+		return nil, err
+	} else {
+		r.Entry = hex.EncodeToString(p)
+	}
+
+	j, err := json.Marshal(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return j, nil
+}
+
+func ComposeChainCommit(pub *[32]byte, pri *[64]byte, c *Chain) ([]byte, error) {
+	type commit struct {
+		CommitChainMsg string
+	}
+
+	buf := new(bytes.Buffer)
+
+	// 1 byte version
+	buf.Write([]byte{0})
+
+	// 6 byte milliTimestamp
+	buf.Write(milliTime())
+
+	e := c.FirstEntry
+
+	// 32 byte ChainID Hash
+	if p, err := hex.DecodeString(c.ChainID); err != nil {
+		return nil, err
+	} else {
+		// double sha256 hash of ChainID
+		buf.Write(shad(p))
+	}
+
+	// 32 byte Weld; sha256(sha256(EntryHash + ChainID))
+	if cid, err := hex.DecodeString(c.ChainID); err != nil {
+		return nil, err
+	} else {
+		s := append(e.Hash(), cid...)
+		buf.Write(shad(s))
+	}
+
+	// 32 byte Entry Hash of the First Entry
+	buf.Write(e.Hash())
+
+	// 1 byte number of Entry Credits to pay
+	if d, err := entryCost(e); err != nil {
+		return nil, err
+	} else {
+		buf.WriteByte(byte(d + 10))
+	}
+
+	// sign the commit
+	sig := ed.Sign(pri, buf.Bytes())
+
+	// 32 byte pubkey
+	buf.Write(pub[:])
+
+	// 64 byte Signature
+	buf.Write(sig[:])
+
+	com := new(commit)
+	com.CommitChainMsg = hex.EncodeToString(buf.Bytes())
+	j, err := json.Marshal(com)
+	if err != nil {
+		return nil, err
+	}
+
+	return j, nil
 }
