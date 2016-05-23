cdde0e5d3fc1cfa91ab698285fa0e5fd46f8d3a5
Michael <michaeljbeam@gmail.com>
2016-05-23 17:51:47 -0500

    rcd type for factoid addresses

---
+++ rcd.go
@@ -0,0 +1,33 @@
+// Copyright 2016 Factom Foundation
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package factom
+
+import(
+	ed "github.com/FactomProject/ed25519"
+)
+
+type RCD interface {
+	Type() byte
+	Hash() []byte
+}
+
+type RCD1 struct {
+	pub *[ed.PublicKeySize]byte
+}
+
+func NewRCD1() *RCD1 {
+	r := new(RCD1)
+	r.pub = new([ed.PublicKeySize]byte)
+	return r
+}
+
+func (r *RCD1) Type() byte {
+	return byte(1)
+}
+
+func (r *RCD1) Hash() []byte {
+	p := append([]byte{r.Type()}, r.Hash()...)
+	return shad(p)
+}
