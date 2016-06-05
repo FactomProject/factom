199b40f9b3204b9858379f2dc9f7f79bc7518d0c
Michael <michaeljbeam@gmail.com>
2016-06-04 19:05:20 -0500

    added GetFee function

---
+++ fee.go
@@ -0,0 +1,11 @@
+// Copyright 2015 Factom Foundation
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package factom
+
+import ()
+
+type FeeResponse struct {
+	Fee int64 `json:"fee"`
+}
