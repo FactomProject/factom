043fd670259b8d23516b1b4282e7897314da80f8
Michael <michaeljbeam@gmail.com>
2016-06-05 16:56:36 -0500

    more transaction funcs

--- wallet/transaction.go
+++ wallet/transaction.go
@@ -6,14 +6,16 @@ package wallet
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/FactomProject/btcutil/base58"
+	ed "github.com/FactomProject/ed25519"
 	"github.com/FactomProject/factoid"
 	"github.com/FactomProject/factom"
 )
 
 var (
-	ErrTXExists = errors.New("wallet: Transaction name already exists")
+	ErrTXExists    = errors.New("wallet: Transaction name already exists")
 	ErrTXNotExists = errors.New("wallet: Transaction name was not found")
 )
 
@@ -35,14 +37,18 @@ func (w *Wallet) DeleteTransaction(name string) error {
 	return nil
 }
 
-func (w *Wallet) AddInput (name string, address *factom.FactoidAddress, amount uint64) error {
+func (w *Wallet) AddInput(name, address string, amount uint64) error {
 	if _, exists := w.transactions[name]; !exists {
 		return ErrTXNotExists
 	}
 	trans := w.transactions[name]
 
-	adr := factoid.NewAddress(address.RCDHash())
-	
+	a, err := w.GetFCTAddress(address)
+	if err != nil {
+		return err
+	}
+	adr := factoid.NewAddress(a.RCDHash())
+
 	// First look if this is really an update
 	for _, input := range trans.GetInputs() {
 		if input.GetAddress().IsSameAs(adr) {
@@ -57,14 +63,12 @@ func (w *Wallet) AddInput (name string, address *factom.FactoidAddress, amount u
 	return nil
 }
 
-// TODO func (w *Wallet) UpdateInput
-
-func (w *Wallet) AddOutput (name, address string, amount uint64) error {
+func (w *Wallet) AddOutput(name, address string, amount uint64) error {
 	if _, exists := w.transactions[name]; !exists {
 		return ErrTXNotExists
 	}
 	trans := w.transactions[name]
-	
+
 	if !factom.IsValidAddress(address) {
 		return errors.New("Invalid Address")
 	}
@@ -72,35 +76,179 @@ func (w *Wallet) AddOutput (name, address string, amount uint64) error {
 	adr := factoid.NewAddress(base58.Decode(address)[2:34])
 
 	trans.AddOutput(adr, amount)
-	
+
 	return nil
 }
 
-func (w *Wallet) AddECOutput (name, address string, amount uint64) error {
+func (w *Wallet) AddECOutput(name, address string, amount uint64) error {
 	if _, exists := w.transactions[name]; !exists {
 		return ErrTXNotExists
 	}
 	trans := w.transactions[name]
-	
+
 	if !factom.IsValidAddress(address) {
 		return errors.New("Invalid Address")
 	}
-	
+
 	adr := factoid.NewAddress(base58.Decode(address)[2:34])
 
 	trans.AddECOutput(adr, amount)
-	
+
 	return nil
 }
 
-//func (w *Wallet) AddFee
-//
-//func (w *Wallet) SubFee
-//
-//func (w *Wallet) SignTransaction
-//
-////func (w *Wallet) SendTransaction
+func (w *Wallet) AddFee(name, address string) (uint64, error) {
+	if _, exists := w.transactions[name]; !exists {
+		return 0, ErrTXNotExists
+	}
+	trans := w.transactions[name]
+
+	{
+		ins, err := trans.TotalInputs()
+		if err != nil {
+			return 0, err
+		}
+		outs, err := trans.TotalOutputs()
+		if err != nil {
+			return 0, err
+		}
+		ecs, err := trans.TotalECs()
+		if err != nil {
+			return 0, err
+		}
+
+		if ins != outs+ecs {
+			return 0, fmt.Errorf("Inputs and outputs don't add up")
+		}
+	}
+
+	fee, err := factom.GetFee()
+	if err != nil {
+		return 0, err
+	}
+
+	transfee, err := trans.CalculateFee(uint64(fee))
+	if err != nil {
+		return 0, err
+	}
+
+	a, err := w.GetFCTAddress(address)
+	if err != nil {
+		return 0, err
+	}
+	adr := factoid.NewAddress(a.RCDHash())
+
+	for _, input := range trans.GetInputs() {
+		if input.GetAddress().IsSameAs(adr) {
+			amt, err := factoid.ValidateAmounts(input.GetAmount(), transfee)
+			if err != nil {
+				return 0, err
+			}
+			input.SetAmount(amt)
+			return transfee, nil
+		}
+	}
+	return 0, fmt.Errorf("%s is not an input to the transaction.", address)
+}
+
+func (w *Wallet) SubFee(name, address string) (uint64, error) {
+	if _, exists := w.transactions[name]; !exists {
+		return 0, ErrTXNotExists
+	}
+	trans := w.transactions[name]
+	
+	if !factom.IsValidAddress(address) {
+		return 0, errors.New("Invalid Address")
+	}
+
+	{
+		ins, err := trans.TotalInputs()
+		if err != nil {
+			return 0, err
+		}
+		outs, err := trans.TotalOutputs()
+		if err != nil {
+			return 0, err
+		}
+		ecs, err := trans.TotalECs()
+		if err != nil {
+			return 0, err
+		}
+
+		if ins != outs+ecs {
+			return 0, fmt.Errorf("Inputs and outputs don't add up")
+		}
+	}
+
+	fee, err := factom.GetFee()
+	if err != nil {
+		return 0, err
+	}
+
+	transfee, err := trans.CalculateFee(uint64(fee))
+	if err != nil {
+		return 0, err
+	}
+
+	
+	adr := factoid.NewAddress(base58.Decode(address)[2:34])
+
+	for _, output := range trans.GetOutputs() {
+		if output.GetAddress().IsSameAs(adr) {
+			output.SetAmount(output.GetAmount() - transfee)
+			return transfee, nil
+		}
+	}
+	return 0, fmt.Errorf("%s is not an output to the transaction.", address)
+}
+
+func (w *Wallet) SignTransaction(name string) error {
+	if _, exists := w.transactions[name]; !exists {
+		return ErrTXNotExists
+	}
+	trans := w.transactions[name]
+
+	data, err := trans.MarshalBinarySig()
+	if err != nil {
+		return err
+	}
+
+	var errMsg []byte
+	for i, rcd := range trans.GetRCDs() {
+		rcd1, ok := rcd.(*factoid.RCD_1)
+		if ok {
+			pub := rcd1.GetPublicKey()
+			key := base58.CheckEncodeWithVersionBytes(pub[:], 0x5f, 0xb1)
+			adr, err := w.GetFCTAddress(key)
+			if err != nil {
+				errMsg = append(errMsg,
+					[]byte("Do not have the private key for: "+
+						factoid.ConvertFctAddressToUserStr(factoid.NewAddress(pub))+"\n")...)
+			} else {
+				sec := new([factoid.SIGNATURE_LENGTH]byte)
+				copy(sec[:], adr.SecBytes())
+				bsig := ed.Sign(sec, data)
+				sig := new(factoid.Signature)
+				sig.SetSignature(bsig[:])
+				sigblk := new(factoid.SignatureBlock)
+				sigblk.AddSignature(sig)
+				trans.SetSignatureBlock(i, sigblk)
+			}
+		}
+	}
+	if errMsg != nil {
+		return errors.New(string(errMsg))
+	}
+
+	return nil
+}
+
+func (w *Wallet) GetTransactions() map[string]factoid.ITransaction {
+	return w.transactions
+}
+
+// TODO ---
 //
 //func (w *Wallet) ComposeTransaction
 //
-//func (w *Wallet) ListTransactions
+//func (w *Wallet) SendTransaction
