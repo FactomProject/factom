a19dc3f08d2fef1bff7119d310bc3e3e6aef0202
Michael <michaeljbeam@gmail.com>
2016-07-13 16:50:47 -0500

    added netki name resolution for addresses

--- resolve.go
+++ resolve.go
@@ -4,7 +4,32 @@
 
 package factom
 
-// TODO - stub until netki resolution is re-integrated
+import (
+	"fmt"
+	
+	netki "github.com/FactomProject/netki-go-partner-client"
+)
+
+func ResolveDnsName(addr string) (string, string, error) {
+	fct, ferr := netki.WalletNameLookup(addr, "fct")
+	ec, eerr := netki.WalletNameLookup(addr, "fec")
+	if ferr != nil && eerr != nil {
+		return fct, ec, fmt.Errorf("%s\n%s", ferr, eerr)
+	}
+	return fct, ec, nil
+}
+
 func GetDnsBalance(addr string) (int64, int64, error) {
-	return 0, 0, nil
+	fct, ec, err := ResolveDnsName(addr)
+	if err != nil {
+		return -1, -1, err
+	}
+
+	f, err1 := GetFactoidBalance(fct)
+	e, err2 := GetECBalance(ec)
+	if err1 != nil || err2 != nil {
+		return f, e, fmt.Errorf("%s\n%s\n", err1, err2)
+	}
+
+	return f, e, nil
 }
