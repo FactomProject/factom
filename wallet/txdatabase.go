feb46679f26cad98ebc1b365aa4600616501ab96
Michael <michaeljbeam@gmail.com>
2016-08-15 15:02:30 -0500

    added get tx by address filter

--- wallet/txdatabase.go
+++ wallet/txdatabase.go
@@ -117,63 +117,29 @@ func (db *TXDatabaseOverlay) GetAllTXs() ([]interfaces.ITransaction, error) {
 // GetTXAddress returns a list of all transactions in the history of Factom that
 // include a specific address.
 func (db *TXDatabaseOverlay) GetTXAddress(adr string) ([]interfaces.ITransaction, error) {
-	// update the database and get the newest fblock
-	newest, err := db.update()
+	filtered := make([]interfaces.ITransaction, 0)
+
+	txs, err := db.GetAllTXs()
 	if err != nil {
 		return nil, err
 	}
-	fblock, err := db.GetFBlock(newest)
-	if err != nil {
-		return nil, err
-	}
-
-	txs := make([]interfaces.ITransaction, 0)
-
-	prevmr := fblock.GetPrevKeyMR().String()
-	for prevmr != factom.ZeroHash {
-		// get all of the txs where an input or output matches the address.
-		for _, tx := range fblock.GetTransactions() {
-			for _, in := range tx.GetInputs() {
-				if primitives.ConvertFctAddressToUserStr(
-					in.GetAddress()) == adr {
-					txs = append(txs, tx)
-				}
-			}
-			for _, out := range tx.GetOutputs() {
-				if primitives.ConvertFctAddressToUserStr(
-					out.GetAddress()) == adr {
-					txs = append(txs, tx)
-				}
-			}
-		}
-
-		// get the previous block
-		fblock, err = db.GetFBlock(prevmr)
-		if err != nil {
-			return nil, err
-		} else if fblock == nil {
-			return nil, fmt.Errorf("Missing fblock in database: %s", prevmr)
-		}
-		prevmr = fblock.GetPrevKeyMR().String()
-	}
-
-	// get the transactions from the last block
-	for _, tx := range fblock.GetTransactions() {
+	
+	for _, tx := range txs {
 		for _, in := range tx.GetInputs() {
 			if primitives.ConvertFctAddressToUserStr(
 				in.GetAddress()) == adr {
-				txs = append(txs, tx)
+				txs = append(filtered, tx)
 			}
 		}
 		for _, out := range tx.GetOutputs() {
 			if primitives.ConvertFctAddressToUserStr(
 				out.GetAddress()) == adr {
-				txs = append(txs, tx)
+				txs = append(filtered, tx)
 			}
 		}
 	}
-
-	return txs, nil
+	
+	return filtered, nil
 }
 
 // GetFBlock retrives a Factoid Block from Factom
