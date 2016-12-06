58becb8c93c82aea6cfef59b4bd66c29ee47022b
Michael <michaeljbeam@gmail.com>
2016-12-06 11:21:22 -0600

    fixed fees req printing

--- transaction.go
+++ transaction.go
@@ -70,7 +70,9 @@ func (tx *Transaction) String() (s string) {
 		)
 	}
 	s += fmt.Sprintln("FeesPaid:", factoshiToFactoid(tx.FeesPaid))
-	s += fmt.Sprintln("FeesRequired:", factoshiToFactoid(tx.FeesRequired))
+	if tx.FeesRequired != 0 {
+		s += fmt.Sprintln("FeesRequired:", factoshiToFactoid(tx.FeesRequired))
+	}
 	s += fmt.Sprintln("Signed:", tx.IsSigned)
 
 	return s
