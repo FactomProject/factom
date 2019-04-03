1e96f25bb0593ac3c4b82aa4163ef97b9b569ff1
Michael <michaeljbeam@gmail.com>
2019-04-03 15:42:20 -0500

    rebase from FD-863

---
+++ currentminute_test.go
@@ -0,0 +1,21 @@
+// Copyright 2016 Factom Foundation
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package factom_test
+
+import (
+	"testing"
+
+	. "github.com/FactomProject/factom"
+)
+
+// TestGetCurrentMinute relies on having a running factom daemon to provide an
+// api endpoint at localhost:8088
+func TestGetCurrentMinute(t *testing.T) {
+	min, err := GetCurrentMinute()
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(min.String())
+}
