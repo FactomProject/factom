4ce51551daa384849e143b5ef6b43e713fb24fc8
Michael <michaeljbeam@gmail.com>
2019-04-23 12:05:18 -0500

    documentation for eblock.go

--- eblock_test.go
+++ eblock_test.go
@@ -15,7 +15,23 @@ import (
 )
 
 func TestGetEBlock(t *testing.T) {
-	simlatedFactomdResponse := `{"jsonrpc":"2.0","id":0,"result":{"header":{"blocksequencenumber":35990,"chainid":"df3ade9eec4b08d5379cc64270c30ea7315d8a8a1a69efe2b98a60ecdd69e604","prevkeymr":"7bd1725aa29c988f8f3486512a01976807a0884d4c71ac08d18d1982d905a27a","timestamp":1487042760,"dbheight":75893},"entrylist":[{"entryhash":"cefd9554e9d89132a327e292649031e7b6ccea1cebd80d8a4722e56d0147dd58","timestamp":1487043240},{"entryhash":"61a7f9256f330e50ddf92b296c00fa679588854affc13c380e9945b05fc8e708","timestamp":1487043240}]}}`
+	simlatedFactomdResponse := `{"jsonrpc":"2.0","id":0,"result":{
+		"header":{
+			"blocksequencenumber":35990,
+			"chainid":"df3ade9eec4b08d5379cc64270c30ea7315d8a8a1a69efe2b98a60ecdd69e604",
+			"prevkeymr":"7bd1725aa29c988f8f3486512a01976807a0884d4c71ac08d18d1982d905a27a",
+			"timestamp":1487042760,
+			"dbheight":75893
+		},
+		"entrylist":[
+			{
+				"entryhash":"cefd9554e9d89132a327e292649031e7b6ccea1cebd80d8a4722e56d0147dd58",
+				"timestamp":1487043240
+			},{
+				"entryhash":"61a7f9256f330e50ddf92b296c00fa679588854affc13c380e9945b05fc8e708","timestamp":1487043240
+			}
+		]
+	}}`
 
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
