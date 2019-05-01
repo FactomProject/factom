0502f1e3b0f9a05233d96746358522dabc5170ca
Michael <michaeljbeam@gmail.com>
2019-05-01 11:57:14 -0500

    added top level package documentation

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Copyright 2016 Factom Foundation
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+/*
+Package factom provides a Golang client implementation of the factomd and factom-walletd APIs.
+*/
+package factom
