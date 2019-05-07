1119a9b869f60eb7d682cac021a4879dcf9c43a9
Michael <michaeljbeam@gmail.com>
2019-05-07 15:38:04 -0500

    string method for properties

--- properties.go
+++ properties.go
@@ -6,11 +6,12 @@ package factom
 
 import (
 	"encoding/json"
+	"fmt"
 )
 
 // PropertiesResponse represents properties of the running factomd and factom
 // wallet.
-type PropertiesResponse struct {
+type Properties struct {
 	FactomdVersion       string `json:"factomdversion"`
 	FactomdVersionErr    string `json:"factomdversionerr"`
 	FactomdAPIVersion    string `json:"factomdapiversion"`
@@ -21,11 +22,34 @@ type PropertiesResponse struct {
 	WalletAPIVersionErr  string `json:"walletapiversionerr"`
 }
 
+func (p *Properties) String() string {
+	var s string
+
+	if p.FactomdVersionErr != "" {
+		s += fmt.Sprintln("FactomdVersionErr:", p.FactomdVersionErr)
+	}
+	s += fmt.Sprintln("FactomdVersion:", p.FactomdVersion)
+	if p.FactomdAPIVersionErr != "" {
+		s += fmt.Sprintln("FactomdAPIVersionErr:", p.FactomdAPIVersionErr)
+	}
+	s += fmt.Sprintln("FactomdAPIVersion:", p.FactomdAPIVersion)
+	if p.WalletVersionErr != "" {
+		s += fmt.Sprintln("WalletVersionErr:", p.WalletVersionErr)
+	}
+	s += fmt.Sprintln("WalletVersion:", p.WalletVersion)
+	if p.WalletAPIVersionErr != "" {
+		s += fmt.Sprintln("WalletAPIVersionErr:", p.WalletAPIVersionErr)
+	}
+	s += fmt.Sprintln("WalletAPIVersion:", p.WalletAPIVersion)
+
+	return s
+}
+
 // GetProperties requests various properties of the factomd and factom wallet
 // software and API versions.
-func GetProperties() (*PropertiesResponse, error) {
+func GetProperties() (*Properties, error) {
 	// get properties from the factom API and the wallet API
-	props := new(PropertiesResponse)
+	props := new(Properties)
 	// wprops := new(PropertiesResponse)
 	req := NewJSON2Request("properties", APICounter(), nil)
 	wreq := NewJSON2Request("properties", APICounter(), nil)
@@ -42,7 +66,7 @@ func GetProperties() (*PropertiesResponse, error) {
 	}
 
 	wresp, werr := walletRequest(wreq)
-	wprops := new(PropertiesResponse)
+	wprops := new(Properties)
 	if werr != nil {
 		props.WalletVersionErr = werr.Error()
 		return props, werr
