f879196d120746c7d668af59a70ffbaff1c66082
Michael <michaeljbeam@gmail.com>
2019-05-15 15:25:09 -0500

    updates for tps test

--- tps_test.go
+++ tps_test.go
@@ -5,12 +5,32 @@
 package factom_test
 
 import (
-	"testing"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 
 	. "github.com/FactomProject/factom"
+
+	"testing"
 )
 
 func TestGetTPS(t *testing.T) {
+	factomdResponse := `{
+	   "jsonrpc": "2.0",
+	   "id": 1,
+	   "result": {
+	      "totaltxrate": 314.1592,
+	      "instanttxrate": 271.828
+	   }
+	}`
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, factomdResponse)
+	}))
+	defer ts.Close()
+
+	SetFactomdServer(ts.URL[7:])
+
 	instant, total, err := GetTPS()
 	if err != nil {
 		t.Error(err)
