44865e44f87fa14d0b8ba7ec580f78f7ea79104e
WhoSoup <34172041+WhoSoup@users.noreply.github.com>
2020-08-20 04:28:33 +0200

    Add support for transactions (#141)
    * return useful results from pending transactions
    
    * expand fblock transactions
    
    * add support for dbheight
    
    * capitalization, unit test, time conversion
    
    Co-authored-by: Brian Deery <carryforward@users.noreply.github.com>

---
+++ transaction_pending.go
@@ -0,0 +1,40 @@
+package factom
+
+import (
+	"encoding/json"
+)
+
+// PendingTransaction is a single transaction returned by the pending-transaction API call
+type PendingTransaction struct {
+	TxID      string `json:"transactionid"`
+	DBHeight  uint32 `json:"dbheight"`
+	Status    string `json:"status"`
+	Fees      uint64 `json:"fees"`
+	Inputs    []TransactionAddress
+	Outputs   []TransactionAddress
+	ECOutputs []TransactionAddress
+}
+
+// GetPendingTransactions requests a list of transactions that have been
+// submitted to the Factom Network, but have not yet been included in a Factoid
+// Block.
+func GetPendingTransactions() ([]PendingTransaction, error) {
+	req := NewJSON2Request("pending-transactions", APICounter(), nil)
+	resp, err := factomdRequest(req)
+
+	if err != nil {
+		return nil, err
+	}
+	if resp.Error != nil {
+		return nil, resp.Error
+	}
+
+	transList := resp.JSONResult()
+
+	var res []PendingTransaction
+	if err := json.Unmarshal(transList, &res); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
